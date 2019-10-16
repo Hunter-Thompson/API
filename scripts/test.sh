bddaf9426ac4a8838abd19f59767c06f851fb6a5
Hunter Thompson <aatman@auroville.org.in>
2019-10-16 21:13:10 +0530

    run tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 curl -v --data "firstname=hunterasd&lastname=thompsonasd&barid=12312&state=urmumasd" http://localhost:3000/createattorney
+
+
+wget --post-data "firstname=hunterasdss&lastname=thompsosssnasd&barid=12312312&state=urmumasasdd" http://localhost:3000/createattorney
+
 curl -v http://localhost:3000/attorney
+
+wget -v http://localhost:3000/attorney
