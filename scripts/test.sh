021d25e05ec5f1eda47dc39ceeb436c3149e1ba7
Hunter Thompson <aatman@auroville.org.in>
2019-10-17 18:00:26 +0530

    nginx added

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-curl -v --data "firstname=hunterasd&lastname=thompsonasd&barid=12312&state=urmumasd" http://localhost:3000/createattorney
-
-
 wget --post-data "firstname=hunterasdss&lastname=thompsosssnasd&barid=12312312&state=urmumasasdd" http://localhost:3000/createattorney
 
 curl -v http://localhost:3000/attorney
