fe5ccd6db4914ff565daa5603a8268d9676dd4ea
Hunter Thompson <aatman@auroville.org.in>
2019-10-18 06:22:57 +0530

    final tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
+sleep 10
+
 wget --post-data "firstname=hunterasdss&lastname=thompsosssnasd&barid=12312312&state=urmumasasdd" http://localhost:3050/api/createattorney
 
 curl -v http://localhost:3050/api/attorney
